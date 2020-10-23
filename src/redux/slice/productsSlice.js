e7bbbd7d312aa3c33a726d6fb393d13dd132b3fb
Peanut1004 <15i1phamngocnhat@gmail.com>
2020-10-23 08:48:09 +0700

    admin

--- src/redux/slice/productsSlice.js
+++ src/redux/slice/productsSlice.js
@@ -1,10 +1,13 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import productsApi from "../../api/productsApi";
 
-export const getProducts = createAsyncThunk("getProducts", async () => {
-  const currentProducts = await productsApi.getProducts();
-  return currentProducts;
-});
+export const getProducts = createAsyncThunk(
+  "getProducts",
+  async (categories) => {
+    const currentProducts = await productsApi.getProducts(categories);
+    return currentProducts;
+  }
+);
 
 export const getProductsHome = createAsyncThunk("getProductsHome", async () => {
   const currentProductsHome = await productsApi.getProductsHome();
@@ -19,15 +22,30 @@ export const getProductDetail = createAsyncThunk(
   }
 );
 
+// export const getCategoriesProduct = createAsyncThunk(
+//   "getCategoriesProduct",
+//   async (categories) => {
+//     const currentCategoriesProduct = await productsApi.getCategoriesProduct(
+//       categories
+//     );
+//     return currentCategoriesProduct;
+//   }
+// );
+
 const productsSlice = createSlice({
   name: "productsSlice",
   initialState: {
     products: [],
     productDetail: [],
+    // categories: "",
     loading: false,
     error: "",
   },
   reducers: {
+    removeProduct: (state, action) => {
+      const removeProductId = action.payload;
+      state.products = state.products.filter((e) => e.id !== removeProductId);
+    },
     getProductDetailEmpty: (state, action) => {
       state.productDetail = [];
     },
@@ -36,25 +54,22 @@ const productsSlice = createSlice({
     [getProducts.pending]: (state) => {
       state.loading = true;
     },
-
     [getProducts.rejected]: (state, action) => {
       state.error = action.error;
       state.loading = false;
     },
-
     [getProducts.fulfilled]: (state, action) => {
       state.products = action.payload;
       state.loading = false;
     },
+
     [getProductsHome.pending]: (state) => {
       state.loading = true;
     },
-
     [getProductsHome.rejected]: (state, action) => {
       state.error = action.error;
       state.loading = false;
     },
-
     [getProductsHome.fulfilled]: (state, action) => {
       state.products = action.payload;
       state.loading = false;
@@ -63,19 +78,29 @@ const productsSlice = createSlice({
     [getProductDetail.pending]: (state) => {
       state.loading = true;
     },
-
     [getProductDetail.rejected]: (state, action) => {
       state.error = action.error;
       state.loading = false;
     },
-
     [getProductDetail.fulfilled]: (state, action) => {
       state.loading = false;
       state.productDetail.push(action.payload);
     },
+
+    // [getCategoriesProduct.pending]: (state) => {
+    //   state.loading = true;
+    // },
+    // [getCategoriesProduct.rejected]: (state, action) => {
+    //   state.error = action.error;
+    //   state.loading = false;
+    // },
+    // [getCategoriesProduct.fulfilled]: (state, action) => {
+    //   state.loading = false;
+    //   state.productDetail.push(action.payload);
+    // },
   },
 });
 
 const { reducer: productsReducer, actions } = productsSlice;
-export const { getProductDetailEmpty } = actions;
+export const { removeProduct, getProductDetailEmpty } = actions;
 export default productsReducer;
