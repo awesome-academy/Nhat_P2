5db73a54adbecddb784fcb04dd9cd159c06bd2e7
Peanut1004 <15i1phamngocnhat@gmail.com>
2020-10-23 09:14:13 +0700

    admin

--- src/routers.js
+++ src/routers.js
@@ -5,7 +5,7 @@ import Register from "./components/pages/Register";
 import Home from "./components/pages/Home";
 // import ListProduct from "./components/pages/ListProduct";
 // import ProductDetail from "./components/pages/ProductDetail";
-// import AdminProductManagement from "./components/pages/AdminProductManagement";
+import AdminProductManagement from "./components/pages/AdminProductManagement";
 import AdminUsersManagement from "./components/pages/AdminUserManagement";
 // import AdminRevenueManagement from "./components/pages/AdminRevenueManagement";
 
@@ -20,16 +20,16 @@ const routers = [
     exact: true,
     main: () => <Register />,
   },
-  // {
-  //   path: "/admin-page",
-  //   exact: true,
-  //   main: () => <AdminProductManagement />,
-  // },
-  // {
-  //   path: "/admin-product-management",
-  //   exact: true,
-  //   main: () => <AdminProductManagement />,
-  // },
+  {
+    path: "/admin-page",
+    exact: true,
+    main: () => <AdminProductManagement />,
+  },
+  {
+    path: "/admin-product-management",
+    exact: true,
+    main: () => <AdminProductManagement />,
+  },
   {
     path: "/admin-user-management",
     exact: true,
