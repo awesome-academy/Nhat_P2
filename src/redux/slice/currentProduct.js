d29001195ca56f8d84dde44f316bad9a931c4466
Peanut1004 <15i1phamngocnhat@gmail.com>
2020-10-23 09:23:31 +0700

    product-list-page

--- src/redux/slice/currentProduct.js
+++ src/redux/slice/currentProduct.js
@@ -17,14 +17,19 @@ function getListProductLocalStorage() {
 const currentProduct = createSlice({
   name: "currentProduct",
   initialState: {
-    listProduct: getListProductLocalStorage(),
+    listProduct: [],
     totalPayment: 0,
-    totalProduct: 0,
+    currentPayment: "",
+    categories: "",
+    numberOfProduct: 0,
+    totalOfProduct: 0,
+    payment: "",
   },
   reducers: {
+    getType: (state, action) => {},
     getAddToCart: (state, action) => {
-      // state.numberOfProduct += 1;
-      // localStorage.setItem("numberOfProduct", state.numberOfProduct);
+      state.numberOfProduct += 1;
+      localStorage.setItem("numberOfProduct", state.numberOfProduct);
 
       const index = state.listProduct.findIndex(
         (e) => e.id === action.payload.id
@@ -32,33 +37,47 @@ const currentProduct = createSlice({
 
       if (index === -1) {
         state.listProduct.push(action.payload);
-        state.totalProduct = state.listProduct.map((e) => e.price * e.quantity);
-        const reducer = (ac, cu) => ac + cu;
-        state.totalProduct = state.totalProduct.reduce(reducer);
+        // alert("Bạn muốn mua sản phẩm này");
+
+        state.totalPayment = state.listProduct.map((e) => e.price * e.quantity);
+        state.totalPayment = state.totalPayment.reduce((ac, cu) => ac + cu, 0);
       } else {
         state.listProduct[index].quantity += 1;
-        state.totalProduct = state.listProduct.map((e) => e.price * e.quantity);
-        const reducer = (ac, cu) => ac + cu;
-        state.totalProduct = state.totalProduct.reduce(reducer);
+
+        state.totalPayment = state.listProduct.map((e) => e.price * e.quantity);
+        state.totalPayment = state.totalPayment.reduce((ac, cu) => ac + cu, 0);
       }
-      setItemLocalStorage(state.listProduct);
-      localStorage.setItem("totalProduct", JSON.stringify(state.totalProduct));
+      localStorage.setItem("listProduct", JSON.stringify(state.totalPayment));
+      localStorage.setItem("totalPayment", JSON.stringify(state.totalPayment));
     },
     getRemoveProduct: (state, action) => {
       state.listProduct = state.listProduct.filter(
-        (p) => p.id !== action.payload
+        (e) => e.id !== action.payload.id
       );
+      state.totalOfProduct = action.payload.price * action.payload.quantity;
+      state.totalPayment = state.totalPayment - state.totalOfProduct;
+      state.numberOfProduct = state.numberOfProduct - action.payload.quantity;
+
       localStorage.setItem("totalPayment", JSON.stringify(state.totalPayment));
     },
     changeQuantityProduct: (state, action) => {
       const { id, quantity } = action.payload;
+
       state.listProduct = state.listProduct.map((p) =>
         p.id !== id ? { ...p } : { ...p, quantity }
       );
+
+      state.numberOfProduct = state.listProduct.map((el) => el.quantity);
+      state.numberOfProduct = state.numberOfProduct.reduce((a, b) => a + b, 0);
+
       state.totalPayment = state.listProduct.map((e) => e.price * e.quantity);
-      const reducer = (ac, cu) => ac + cu;
-      state.totalPayment = state.totalPayment.reduce(reducer);
-      setItemLocalStorage(state.listProduct);
+      state.totalPayment = state.totalPayment.reduce((ac, cu) => ac + cu, 0);
+
+      localStorage.setItem(
+        "numberOfProduct",
+        JSON.stringify(state.totalPayment)
+      );
+      localStorage.setItem("listProduct", JSON.stringify(state.totalPayment));
       localStorage.setItem("totalPayment", JSON.stringify(state.totalPayment));
     },
     getCancelOrder: (state, action) => {
@@ -66,31 +85,38 @@ const currentProduct = createSlice({
       state.totalPayment = 0;
       localStorage.setItem("totalPayment", state.listProduct);
     },
+    getCurrentPayment: (state, action) => {
+      state.currentPayment = action.payload;
+      localStorage.setItem("payment", state.currentPayment);
+    },
+    getRemoveInCart: (state, action) => {
+      state.listProduct = [];
+      state.totalPayment = 0;
+      state.numberOfProduct = 0;
+      localStorage.setItem("totalPayment", state.listProduct);
+    },
   },
 });
 const { reducer: currentProductReducer, actions } = currentProduct;
+// console.log(currentProduct);
 
 // export const productsSelector = (state) => state.currentProduct.listProduct;
-
 // export const totalProduct = createSelector(
 //   productsSelector,
 //   (products) => products.length
 // );
-
 // export const totalQuantity = createSelector(productsSelector, (items) =>
 //   items.reduce((acc, item) => acc + item.quantity, 0)
 // );
 
-// var result = 0;
-// export const intoMoney = createSelector(productsSelector, (e) =>
-//   e.map((e, i) => (result += e.price * e.quantity))
-// );
-
 export const {
+  getType,
   getAddToCart,
   getRemoveProduct,
   changeQuantityProduct,
   // totalQuantity,
   getCancelOrder,
+  getCurrentPayment,
+  getRemoveInCart,
 } = actions;
 export default currentProductReducer;
